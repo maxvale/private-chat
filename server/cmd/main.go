39f8d74b740847ab6a4ef9fb7642f3eb1b57ef82
Valentin Maksimchuk <Valentin.maksimchuk@ext.hellofresh.com>
2021-09-21 17:49:35 +0300

    Create template for project #2

---
+++ server/cmd/main.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"github.com/go-chi/chi"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+}
---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.16
+
+require (
+	github.com/go-chi/chi v1.5.4
+	google.golang.org/protobuf v1.27.1
+)
